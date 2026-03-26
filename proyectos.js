bfda649d1ca1dd0b2056a6569305c9141ccef732
emito182-droid <emito182@gmail.com>
2026-03-25 22:16:07 -0500

    Add files via upload

---
+++ proyectos.js
@@ -0,0 +1,23 @@
+// Este es el archivo que subes a GitHub
+const DATA_CENTRAL = [
+    {
+        id: ,
+        nombreMaestro: ,
+        concesiones: [
+            {
+                nombre: 
+                codigo: 
+                provincia: 
+                canton: 
+                parroquia: 
+                tipo: 
+                superficie: 
+                regimen: 
+                periodo: 
+                minerales: 
+                lat: 
+                lng: 
+            }
+        ]
+    }
+];
