c7645bd932f144a4546e396bb3da6381e7b1e6e0
emito182-droid <emito182@gmail.com>
2026-03-25 22:44:02 -0500

    Add files via upload

---
+++ proyectos.js
@@ -0,0 +1,24 @@
+const DATA_CENTRAL = [
+    {
+        "id": 1711400000000,
+        "nombreMaestro": ,
+        "concesiones": [
+            {
+                "nombre": 
+                "codigo": 
+                "provincia": 
+                "canton":
+                "parroquia": ,
+                "tipo": 
+                "superficie": ,
+                "regimen": 
+                "periodo": ,
+                "minerales": [
+                    
+                ],
+                "lat": 
+                "lng": 
+            }
+        ]
+    }
+];
