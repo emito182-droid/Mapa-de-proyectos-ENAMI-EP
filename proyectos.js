2cd42292995f9502466bcf40adfb7b6c29a53f23
emito182-droid <emito182@gmail.com>
2026-03-25 22:49:45 -0500

    Add files via upload

---
+++ proyectos.js
@@ -0,0 +1,68 @@
+const DATA_CENTRAL = [
+    {
+        "id": 1711400000000,
+        "nombreMaestro": "PROYECTO INICIAL ENAMI",
+        "concesiones": [
+            {
+                "nombre": "CONCESIÓN EJEMPLO",
+                "codigo": "001-A",
+                "provincia": "PICHINCHA",
+                "canton": "QUITO",
+                "parroquia": "CENTRO",
+                "tipo": "METÁLICA",
+                "superficie": "100",
+                "regimen": "PEQUEÑA MINERÍA",
+                "periodo": "EXPLORACIÓN INICIAL",
+                "minerales": [
+                    "ORO"
+                ],
+                "lat": -0.1807,
+                "lng": -78.4678
+            }
+        ]
+    },
+    {
+        "id": 1774496332986,
+        "nombreMaestro": "TOLA NORTE",
+        "concesiones": [
+            {
+                "nombre": "LA TOLA NORTE",
+                "codigo": "402994",
+                "provincia": "ESMERALDAS",
+                "canton": "ELOY ALFARO",
+                "parroquia": "LA TOLA",
+                "tipo": "METÁLICA",
+                "superficie": "4648",
+                "regimen": "GRAN MINERÍA",
+                "periodo": "EXPLORACIÓN AVANZADA",
+                "minerales": [
+                    "TITANO FERROSO"
+                ],
+                "lat": 0.9592,
+                "lng": -79.65397
+            }
+        ]
+    },
+    {
+        "id": 1774496931207,
+        "nombreMaestro": "UNACOTA",
+        "concesiones": [
+            {
+                "nombre": "UNACOTA",
+                "codigo": "200990",
+                "provincia": "COTOPAXI",
+                "canton": "COTOPAXI",
+                "parroquia": "COTOPAXI",
+                "tipo": "METÁLICA",
+                "superficie": "2.608",
+                "regimen": "GRAN MINERÍA",
+                "periodo": "EXPLORACIÓN INICIAL",
+                "minerales": [
+                    "CALIZA"
+                ],
+                "lat": -0.96167,
+                "lng": -78.8975
+            }
+        ]
+    }
+];
