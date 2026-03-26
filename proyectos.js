f1fd7e0edf3e111abe0d6f2995ac89000e424a38
emito182-droid <emito182@gmail.com>
2026-03-25 22:39:59 -0500

    Add files via upload

---
+++ proyectos.js
@@ -0,0 +1,24 @@
+const DATA_CENTRAL = [
+    {
+        "id": 1711400000000,
+        "nombreMaestro": "PROYECTO INICIAL ENAMI",
+        "concesiones": [
+            {
+                "nombre": "CONCESIÓN EJEMPLO",
+                "codigo": "001-A",
+                "provincia": "PICHINCHA",
+                "canton": "QUITO",
+                "parroquia": "CENTRO",
+                "tipo": "METÁLICA",
+                "superficie": "100",
+                "regimen": "PEQUEÑA MINERÍA",
+                "periodo": "EXPLORACIÓN INICIAL",
+                "minerales": [
+                    "ORO"
+                ],
+                "lat": -0.1807,
+                "lng": -78.4678
+            }
+        ]
+    }
+];
