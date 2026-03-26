34c4fed114968f8efc4e49c4f62173ef18c74ff0
emito182-droid <emito182@gmail.com>
2026-03-25 21:53:51 -0500

    Add files via upload

---
+++ proyectos.js
@@ -0,0 +1,23 @@
+// Base de datos compartida para ENAMI EP
+const DATA_CENTRAL = [
+    {
+        id: 1711400000000,
+        nombreMaestro: "PROYECTO CASCABEL",
+        concesiones: [
+            {
+                nombre: 
+                codigo: 
+                provincia: 
+                canton: 
+                parroquia: 
+                tipo: 
+                superficie: 
+                regimen: 
+                periodo: 
+                minerales: 
+                lat: 
+                lng: 
+            }
+        ]
+    }
+];
